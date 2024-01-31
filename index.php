c3900c572d8498228970cbd5bf247d54a58a22ea
JhonadaDeLima <jhonatapereira28@gmail.com>
2024-01-31 02:51:53 -0300

    change task style

--- index.php
+++ index.php
@@ -25,102 +25,58 @@ $tasks = $taskDao->select($user->id);
                 <?=$_SESSION['flash'] = null; ?>
             <?php endif; ?>
         </form>
-
-
     </div>
 
+
     <div class="coluns">
-                <div class="action" id="regular"><div class="t"><h3>REGULAR</h3></div></div>
+        <div class="action" id="regular"><div class="t"><h3>REGULAR</h3></div></div>
 
         <?php foreach ($tasks as $item):?>
+            <?php $creat_at = $item['creat_at'];
+                 $date = implode('/', array_reverse(explode('-', $creat_at)));?>
             <div class="regular">
-                <div class="task">  
+
+                <div class="task">
                         <h1 style="display: none;"><?=$item['id'];?></h1>
                         <h3><?=$item['title'];?></h3>
                         <p><?=$item['description'];?></p>
-                        <a href="<?=$base;?>/task_edit.php?id=<?=$item['id'];?>"><img title="iniciar" src="<?=$base;?>/assets/img/iniciar.png"/></a>
-                        <a href="<?=$base;?>/task_finish.php?id=<?=$item['id'];?>"><img title="concluir" src="<?=$base;?>/assets/img/concluir.png"/></a>
-                        <a href="<?=$base;?>/task_delet.php?id=<?=$item['id'];?>"><img title="apagar" src="<?=$base;?>/assets/img/apagar.png"/></a>
-                </div>           
+
+                    <div class="po">
+                        <div class="taskIcons">
+                            <a href="<?=$base;?>/task_edit.php?id=<?=$item['id'];?>"><img title="iniciar" src="<?=$base;?>/assets/img/iniciar.png"/></a>
+                            <a href="<?=$base;?>/task_finish.php?id=<?=$item['id'];?>"><img title="concluir" src="<?=$base;?>/assets/img/concluir.png"/></a>
+                            <a href="<?=$base;?>/task_delet.php?id=<?=$item['id'];?>"><img title="apagar" src="<?=$base;?>/assets/img/apagar.png"/></a>
+                        </div>
+                        <div class="taskDates">
+                            <div class="date" ><b>inicio:</b> <p id="dateStart"><?=$item['creat_at'];?></p></div>
+                            <div class="date" ><b>fim:_</b> <p id="dateDone"><?=$item['finish_at'];?></p></div>
+                        </div> 
+                    </div>
+                </div> 
+                          
             </div>
         <?php endforeach; ?>
-
     </div>
+
 
     <div class="coluns">
         <div class="action" id="importante"><h3>IMPORTANTE</h3></div>
 
-<?php foreach ($tasks as $item):?>
-    <div class="task1">
-                <h1><?=$item['id'];?></h1>
-                <h1><?=$item['title'];?></h1>
-                <h3><?=$item['status'];?></h3>
-                <div class="desc"><p><?=$item['description'];?></p></div>
-                <h3>Inicio: <?=$item['creat_at'];?></h3><h3>Fim: <?=$item['finish_at'];?></h3>
-                <div class="botao"><a href="<?=$base;?>/task_finish.php?id=<?=$item['id'];?>"><button>CONCLUIR</button></a></div>
-                <div class="botao"><a href="<?=$base;?>/task_edit.php?id=<?=$item['id'];?>"><button>EDITAR</button></a></div>
-                <div class="botao"><a href="<?=$base;?>/task_delet.php?id=<?=$item['id'];?>"><button>EXCLUIR</button></a></div>           
     </div>
-<?php endforeach; ?>
 
-</div>
-
-<div class="coluns">
-    <div class="action" id="urgente"><h3>URGENTE</h3></div>
-
-<?php foreach ($tasks as $item):?>
-    <div class="task1">
-                <h1><?=$item['id'];?></h1>
-                <h1><?=$item['title'];?></h1>
-                <h3><?=$item['status'];?></h3>
-                <div class="desc"><p><?=$item['description'];?></p></div>
-                <h3>Inicio: <?=$item['creat_at'];?></h3><h3>Fim: <?=$item['finish_at'];?></h3>
-                <div class="botao"><a href="<?=$base;?>/task_finish.php?id=<?=$item['id'];?>"><button>CONCLUIR</button></a></div>
-                <div class="botao"><a href="<?=$base;?>/task_edit.php?id=<?=$item['id'];?>"><button>EDITAR</button></a></div>
-                <div class="botao"><a href="<?=$base;?>/task_delet.php?id=<?=$item['id'];?>"><button>EXCLUIR</button></a></div>           
+    <div class="coluns">
+        <div class="action" id="urgente"><h3>URGENTE</h3></div>
     </div>
-<?php endforeach; ?>
-
-</div>
 
 
-<div class="coluns">
-    <div class="action" id="andamento"><h3>EM ANDAMENTO</h3></div>
-
-<?php foreach ($tasks as $item):?>
-    <div class="task1">
-                <h1 style="display: none;"><?=$item['id'];?></h1>
-                <h1><?=$item['title'];?></h1>
-                <h3><?=$item['status'];?></h3>
-                <div class="desc"><p><?=$item['description'];?></p></div>
-                <h3>Inicio: <?=$item['creat_at'];?></h3><h3>Fim: <?=$item['finish_at'];?></h3>
-                <div class="botao"><a href="<?=$base;?>/task_finish.php?id=<?=$item['id'];?>"><button>CONCLUIR</button></a></div>
-                <div class="botao"><a href="<?=$base;?>/task_edit.php?id=<?=$item['id'];?>"><button>EDITAR</button></a></div>
-                <div class="botao"><a href="<?=$base;?>/task_delet.php?id=<?=$item['id'];?>"><button>EXCLUIR</button></a></div>           
+    <div class="coluns">
+        <div class="action" id="andamento"><h3>EM ANDAMENTO</h3></div>
     </div>
-<?php endforeach; ?>
-
-</div>
 
 
-<div class="coluns">
-    <div class="action" id="concluido"><h3>CONCLUIDO</h3></div>
-
-<?php foreach ($tasks as $item):?>
-    <div class="task1">
-                <h1><?=$item['id'];?></h1>
-                <h1><?=$item['title'];?></h1>
-                <h3><?=$item['status'];?></h3>
-                <div class="desc"><p><?=$item['description'];?></p></div>
-                <h3>Inicio: <?=$item['creat_at'];?></h3><h3>Fim: <?=$item['finish_at'];?></h3>
-                <div class="botao"><a href="<?=$base;?>/task_finish.php?id=<?=$item['id'];?>"><button>CONCLUIR</button></a></div>
-                <div class="botao"><a href="<?=$base;?>/task_edit.php?id=<?=$item['id'];?>"><button>EDITAR</button></a></div>
-                <div class="botao"><a href="<?=$base;?>/task_delet.php?id=<?=$item['id'];?>"><button>EXCLUIR</button></a></div>           
+    <div class="coluns">
+        <div class="action" id="concluido"><h3>CONCLUIDO</h3></div>
     </div>
-<?php endforeach; ?>
-
-</div>
-
 
 
 
